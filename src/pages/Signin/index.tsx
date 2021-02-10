cc906d5baa18df16e159e4a22e6072adfda7db0e
juliorox666 <juliorox666@gmail.com>
2021-02-10 19:12:46 -0300

    Added more tests and fixed signinpage name

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import MyForm from "components/MyForm";
 import { User } from "services/user";
 
-export const SinginPage: React.FC = () => {
+export const SigninPage: React.FC = () => {
   return (
     <div className="signin-page">
       <MyForm onSubmitHandler={(data: User) => JSON.stringify(data)} />
